8fe06271f6e4336b3388c840cb86bb9b6c8a5b1d
jsen- <max.enhanced@gmail.com>
2017-07-31 06:50:51 +0200

    make string literal use  and  combinators

--- src/parsers/string_literal.ts
+++ src/parsers/string_literal.ts
@@ -2,5 +2,7 @@ import { char, any, Parser } from "..";
 
 export function string_literal(boundary: Parser<string> = char("\""), escape_character: Parser<string> = char("\\")): Parser<string> {
     const escaped_character = escape_character.next(any()).or(any());
-    return boundary.next(escaped_character.untilFollowedBy(boundary)).join();
+    return boundary
+        .next(escaped_character.until(boundary.skip()))
+        .join();
 }
